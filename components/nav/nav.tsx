3e52ad65e39846ede1a742ff09a7a17ee74187c0
Alexi <jemimassyrup@gmail.com>
2024-08-12 21:05:55 -0700

    logo

--- components/nav/nav.tsx
+++ components/nav/nav.tsx
@@ -2,21 +2,20 @@
 
 import { Link } from 'next-view-transitions'
 import React from 'react'
-import { Button } from '../ui/button'
 import { useRouter } from 'next/navigation'
-import { SignIn, SignInButton, SignUpButton, UserButton, useUser } from '@clerk/nextjs'
+import { SignInButton, SignUpButton, UserButton, useUser } from '@clerk/nextjs'
 import Image from 'next/image'
 
 export default function Nav() {
 	const router = useRouter();
 	const { user } = useUser();
 	return (
-	<nav className='flex justify-between h-12 items-center max-w-6xl w-[1152px] m-auto'>
-		<Link href="/" className='relative '>
+	<nav className='flex justify-between h-12 items-center max-w-6xl w-screen m-auto '>
+		<Link href="/" className='relative left-4'>
 			<Image  width={28} height={42} src="/pngs/logo-other.png" alt='logo.png' />
 		</Link>
 
-		<div className='relative flex gap-4  justify-end'>
+		<div className='relative flex gap-4  justify-end right-4'>
 			<Link href="/">Home</Link>
 			<Link href="/">FAQ</Link>
 			<Link href="/contact">Contact</Link>
